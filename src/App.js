21eb7302687a7a57c6894af6cb3aab56f75d55c8
Taylor Roberts <tstarrroberts@gmail.com>
2022-07-11 21:20:32 -0400

    add bracket

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React} from 'react';
 // import Nav from './components/Nav';
 // import About from './components/About';
 // import Project from './components/Project';
