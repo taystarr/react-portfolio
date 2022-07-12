0d08af16659bc791cdd3fcc842842c0ed82b8af7
Taylor Roberts <tstarrroberts@gmail.com>
2022-07-11 21:08:46 -0400

    h1 app

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
 import React from 'react';
-import Nav from './components/Nav';
-import About from './components/About';
-import Project from './components/Project';
-import Contact from './components/Contact';
-import Resume from './components/Resume';
+// import Nav from './components/Nav';
+// import About from './components/About';
+// import Project from './components/Project';
+// import Contact from './components/Contact';
+// import Resume from './components/Resume';
 import './index.css';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 
 function App() {
 
-  let Component
-  // eslint-disable-next-line
-  switch (window.location.pathname) {
-    case "/react-portfolio":
-      Component = About;
-      break;
-      // eslint-disable-next-line
-    case "/about":
-      Component = About;
-      break;
-    case "/portfolio":
-      Component = Project;
-      break;
-    case "/contact":
-      Component = Contact;
-      break;
-    case "/resume":
-      Component = Resume;
-      break;
-  }
+  // let Component
+  // // eslint-disable-next-line
+  // switch (window.location.pathname) {
+  //   case "/react-portfolio":
+  //     Component = About;
+  //     break;
+  //     // eslint-disable-next-line
+  //   case "/about":
+  //     Component = About;
+  //     break;
+  //   case "/portfolio":
+  //     Component = Project;
+  //     break;
+  //   case "/contact":
+  //     Component = Contact;
+  //     break;
+  //   case "/resume":
+  //     Component = Resume;
+  //     break;
+  // }
   return (
     
     <div>
-      <Nav>
+      {/* <Nav>
       </Nav>
-      <Component></Component>
+      <Component></Component> */}
       <main>
         <h1>It would be nice if this worked</h1>
           {/* <About></About>
           <Project></Project> */}
       </main>
-        <Footer />
+        {/* <Footer /> */}
     </div>
   );
 }
